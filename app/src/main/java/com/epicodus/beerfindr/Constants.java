83d0807f6a9df92bbfeb14cfa9b08366aaebbc65
Peter Beach <peterbeach2@gmail.com>
2016-07-18 08:05:08 -0700

    added gradle.properties to git ignore.

---
+++ app/src/main/java/com/epicodus/beerfindr/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.beerfindr;
+
+/**
+ * Created by Peter on 7/17/16.
+ */
+public class Constants {
+    public static final String BREWERY_DB_KEY = BuildConfig.BREWERY_DB_KEY;
+    public static final String BASE_URL = "";
+}
